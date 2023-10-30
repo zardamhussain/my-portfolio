503395ebd67610810044ce4dc772099193fbbb0b
Zardam Hussain <zardamhussain@gmail.com>
2023-10-30 15:28:20 +0530

    portfolio-4

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Lottie from "react-lottie";
 import animationData from "../public/not-found.json";
 
@@ -13,23 +13,9 @@ function NotFound() {
     },
   };
 
-  const [height, setHeight] = useState(window.outerWidth < 500 ? 200 : 400);
-  useEffect(() => {
-    const handleWindowResize = () => {
-        if(window.outerWidth < 500 ) setHeight(200);
-        else setHeight(400);
-      };
-
-      window.addEventListener('resize', handleWindowResize);
-
-      return () => {
-        window.removeEventListener('resize', handleWindowResize);
-      };
-  });
-
   return (
-    <div className="bg-black h-screen flex justify-center items-center w-screen">
-        <Lottie options={defaultOptions}  height={height} width={height}/>
+    <div className="bg-black min-h-screen flex justify-center items-center  min-w-screen">
+        <Lottie options={defaultOptions}  height={'50%'} width={'50%'}/>
     </div>
   )
 }
